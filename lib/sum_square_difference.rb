4076fb44de09753f1e8a1754cc83b18b9a76c03b
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-05 21:46:20 -0400

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,11 +1,13 @@
 # Implement your procedural solution here!
 def square_of_sum(n)
-  sum = (1..n).inject {|sum, i| sum + i}
-  square = sum **
+  sum = (1..n).inject(0) {|sum, i| sum + i}
+  square = sum ** 2
 end
 def sum_of_square(n)
-  (1..n).inject {|sum, i| sum = (i * i)}
+  sum = 0
+  (1..n).each {|i|  sum = (i ** 2) + sum}
+  sum
 end
 def sum_square_difference(n)
-  sum_of_square(n) - square_of_sum(n)
+  square_of_sum(n) - sum_of_square(n)
 end
