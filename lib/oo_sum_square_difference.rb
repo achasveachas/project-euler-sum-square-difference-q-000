6197d8acd434dee953a737c513e0bc35cf071181
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-05 21:54:41 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,4 +1,18 @@
 # Implement your object-oriented solution here!
 class SumSquareDifference
-
+  def initialize(n)
+    @n = n
+  end
+  def sumSquare
+    @sum = 0
+    (1..@n).each {|i|  @sum = (i ** 2) + @sum}
+    @sum
+  end
+  def squareSum
+    @sum = (1..@n).inject(0) {|n, i| n + i}
+    @square = @sum ** 2
+  end
+  def difference
+    squareSum - sumSquare
+  end
 end
